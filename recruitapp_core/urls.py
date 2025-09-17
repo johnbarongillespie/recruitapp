2ab360ead5ca35a28f8476580877f2d4f0d8d86e
John Gillespie <johnbarongillespie@gmail.com>
2025-09-16 20:21:54 -0500

    feat: db-testing

--- recruitapp_core/urls.py
+++ recruitapp_core/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from recruitapp_core import views as agent_views
+from recruiting import views as recruiting_views # <-- UPDATED IMPORT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('agent/', include('recruiting.urls')),
+    path('', include('recruiting.urls')), # <-- UPDATED PATH
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/register/', agent_views.register, name='register'),
+    path('accounts/register/', recruiting_views.register, name='register'), # <-- UPDATED VIEW
     # Add this new path for handling the verification link
-    path('verify/<str:uidb64>/<str:token>/', agent_views.verify_email, name='verify_email'),
+    path('verify/<str:uidb64>/<str:token>/', recruiting_views.verify_email, name='verify_email'), # <-- UPDATED VIEW
 ]
